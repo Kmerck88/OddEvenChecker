86c92c284c957bc750c1b51219ef0f69c61e0706
kmerc <kmerc@MERCK>
2024-06-02 16:51:51 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿/* 
+ * Create & intialize two ints 
+ * Make a variable to work out the reminder 
+ * Output the reminder to the screen 
+ * Change the first int variable to another number
+  * and recalculate the reminder 
+  * out the new reminder to the screen
+ */ 
+
+
